25c5704c4b7901f1f0490f15e2aa920681f68aa9
JavierRodriguezM <71197841+JavierRodriguezM@users.noreply.github.com>
2020-09-18 21:20:33 -0600

    Add files via upload
    Código corregido, sin errores detectables por flake8

--- flake8_errores.py
+++ flake8_errores.py
@@ -1,9 +1,9 @@
-#Función que devuelve todos los divisores de un número dado
+# Función que devuelve todos los divisores de un número dado
 def divisores(n):
-    if isinstance(n, int)==True and n>=1:
-        resultado=[]
-        for a in range(1,n):
-            if n%a==0:
+    if isinstance(n, int) and n >= 1:
+        resultado = []
+        for a in range(1, n):
+            if n % a == 0:
                 resultado.append(a)
         return resultado
     else:
